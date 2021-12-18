87f4a3e08263a73b42a1eaf1fe5d3d7335c72bc8
solsansserri <solsansserri@gmail.com>
2021-12-18 17:06:25 -0300

    nuevo

---
+++ script.js
@@ -0,0 +1,6 @@
+var getData= function() {
+    var nombreyapellido= document.getElementById("nombreyapellido").value;
+    var correoelectronico= document.getElementById("correoelectronico").value;
+    var mensaje= document.getElementById("mensaje").value;
+    console.log(nombreyapellido+correoelectronico+mensaje);
+}
