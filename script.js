7ab7318d4cc967de90f9864e4584e960768f56bb
solsansserri <solsansserri@gmail.com>
2021-12-18 17:44:32 -0300

    nuevo

--- script.js
+++ script.js
@@ -2,5 +2,37 @@ var getData= function() {
     var nombreyapellido= document.getElementById("nombreyapellido").value;
     var correoelectronico= document.getElementById("correoelectronico").value;
     var mensaje= document.getElementById("mensaje").value;
-    console.log(nombreyapellido+correoelectronico+mensaje);
+
+    if(nombreyapellido == "") {
+        document.getElementById("nombreyapellido").focus();
+    } else {
+
+    }
+
+    if(correoelectronico == "") {
+        document.getElementById("correoelectronico").focus();
+    } else {
+        
+    }
+
+    if(mensaje == "") {
+        document.getElementById("mensaje").focus();
+    } else {
+        
+    }
+
+
+    console.log(nombreyapellido+" "+correoelectronico+" "+mensaje);
+
+    document.getElementById("nombreyapellido").value = "";
+    document.getElementById("correoelectronico").value = "";
+    document.getElementById("mensaje").value = "";
+    document.getElementById("nombreyapellido").focus();
 }
+
+function mostrarMensaje() {
+    var mensajes =document.getElementById("botonenviar").value;
+    document.querySelector("h1").textContent = mensajes;
+}
+var formulario =document.querySelector("#formulario");
+botonenviar.addEventListener("input", mostrarMensaje);
